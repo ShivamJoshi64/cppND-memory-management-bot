63f9b52020f9a8590dc53e1a2ebc513cc2ad5772
ShivamJoshi64 <joshishivam64@gmail.com>
2020-10-03 23:42:01 -0400

    all files

---
+++ build/CMakeFiles/membot.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/shivam/Desktop/Memory-Management-Bot/src/chatbot.cpp" "/home/shivam/Desktop/Memory-Management-Bot/build/CMakeFiles/membot.dir/src/chatbot.cpp.o"
+  "/home/shivam/Desktop/Memory-Management-Bot/src/chatgui.cpp" "/home/shivam/Desktop/Memory-Management-Bot/build/CMakeFiles/membot.dir/src/chatgui.cpp.o"
+  "/home/shivam/Desktop/Memory-Management-Bot/src/chatlogic.cpp" "/home/shivam/Desktop/Memory-Management-Bot/build/CMakeFiles/membot.dir/src/chatlogic.cpp.o"
+  "/home/shivam/Desktop/Memory-Management-Bot/src/graphedge.cpp" "/home/shivam/Desktop/Memory-Management-Bot/build/CMakeFiles/membot.dir/src/graphedge.cpp.o"
+  "/home/shivam/Desktop/Memory-Management-Bot/src/graphnode.cpp" "/home/shivam/Desktop/Memory-Management-Bot/build/CMakeFiles/membot.dir/src/graphnode.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "WXUSINGDLL"
+  "_FILE_OFFSET_BITS=64"
+  "__WXGTK__"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/lib/wx/include/gtk3-unicode-3.1"
+  "/usr/local/include/wx-3.1"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
